1360725f4b82b3d637adc4367fa3a6adc5c958ac
Rimjhim Keshari <rimjhimkeshari99@gmail.com>
2026-07-29 14:49:16 +0530

    done

---
+++ day 1/index.js
@@ -0,0 +1,23 @@
+const app=require('./app.js')
+// var a= 10;
+// var b=20;
+// var c=30;
+// console.log(a+b+c);
+// import promptSync from "prompt-sync";
+
+// const prompt = promptSync();
+// var x =prompt("enter the no.");
+// x=Number.parseInt(x);
+// if(x==20){
+//     console.log("x is 20");
+// }
+// else{
+//     console.log("x is not 20");
+// }
+var x=20;
+const arr=[1,2,3,4,5];
+for(var i=0;i<arr.length;i++){
+console.log(arr[i]);
+console.log(arr[2]);
+}
+console.log(app.xz);
