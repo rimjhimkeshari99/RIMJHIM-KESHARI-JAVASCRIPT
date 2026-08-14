82c6dadc049365f768ddd41e496f7a31b6289e24
Rimjhim Keshari <rimjhimkeshari99@gmail.com>
2026-08-14 11:20:46 +0530

    done

---
+++ day 3/studentdetails.js
@@ -0,0 +1,11 @@
+console.log("===student details===")
+var student={
+    name:"rimjhim",
+    id:154,
+    branch:"computer science",
+    department:"aiml"
+}
+console.log("name=",student.name);
+console.log("id=",student.id);
+console.log("branch=",student.branch);
+console.log("department=",student.department);
