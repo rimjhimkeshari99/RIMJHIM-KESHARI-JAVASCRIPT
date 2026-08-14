4d5531660626d29918f9dfb524007aa407ad171d
Rimjhim Keshari <rimjhimkeshari99@gmail.com>
2026-08-14 11:20:38 +0530

    done

---
+++ day 3/calculator.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+console.log("sum=",add(20,30));
+console.log("sub=",sub(20,30));
+console.log("mul=",mul(20,30));
+console.log("div=",div(20,30));
