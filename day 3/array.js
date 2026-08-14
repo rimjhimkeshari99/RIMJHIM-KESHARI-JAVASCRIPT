22f3dd2e9b963fbe90683940a18f7c6632f853ce
Rimjhim Keshari <rimjhimkeshari99@gmail.com>
2026-08-14 11:20:22 +0530

    done

---
+++ day 3/array.js
@@ -0,0 +1,7 @@
+let arr=[10,20,30,40,50];
+let sum=0;
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    sum+=arr[i];
+}
+console.log("sum=",sum);
---
+++ day 3/checknumber.js
@@ -0,0 +1,7 @@
+var num =25
+if(num%2==0){
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
