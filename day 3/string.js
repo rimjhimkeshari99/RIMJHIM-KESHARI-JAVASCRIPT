78a7c58f2381aa2e33c7e6228181a177bff8c777
Rimjhim Keshari <rimjhimkeshari99@gmail.com>
2026-08-19 13:47:07 +0530

    done

---
+++ day 3/string.js
@@ -0,0 +1,22 @@
+let name="rimjhim";
+console.log(name);
+console.log("length="+name.length);
+let str="Welcome JavaScript";
+console.log("uppercase=",str.toUpperCase())
+console.log("lowercase=",str.toLowerCase())
+console.log("character at index 3=",str.charAt(3))
+let message="javascript is good ,javascript helps"
+console.log("index of java=" ,str.indexOf("Java"))
+console.log("last index=",message.lastIndexOf("javascript"))
+console.log("slice=",str.slice(0,7))
+let data="html,css,javasript"
+let result=data.split(",")
+console.log("split=",result)
+let text="i love javascript"
+console.log(text.replace("javascript","java"))
+let first="rimjhim"
+let second="keshari"
+console.log(first.concat(" ",second))
+let course="javascript"
+console.log(course.includes("script"))
+console.log(course.includes("python"))
